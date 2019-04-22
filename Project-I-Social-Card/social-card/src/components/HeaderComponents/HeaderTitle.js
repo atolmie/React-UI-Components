e198dc90bd9dacb2e6598c9ad4e8103ccc2f50f5
atolmie <atolmie22@gmail.com>
2019-04-22 18:22:32 -0500

    update

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,14 @@
 import React from 'react';
 import './Header.css';
+
+function HeaderTitle() {
+    return (
+        <div className="header-title">
+            <div>Lambda School</div>
+            <div className="light-gray">@lambda_school</div>
+            <div className="light-gray">26 Jan</div>
+        </div>
+    )
+}
+
+export default HeaderTitle;
