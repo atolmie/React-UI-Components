05a872b272d25a2e27e5a2b0bb14425e065bc85e
atolmie <atolmie22@gmail.com>
2019-04-22 21:30:40 -0500

    cardbanner done

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
@@ -1,2 +1,21 @@
 import React from 'react';
 import './Card.css';
+import './CardContent';
+import './CardBanner';
+
+// will contain the div w/ border
+// import banner
+// import content
+
+const CardContainer = () => {
+    return (
+      <div className="cardcontainer">
+        <CardContent />
+        <CardBanner />
+      </div>
+    );
+  };
+  
+  export default CardContainer;
+  
+  
